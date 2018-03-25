6469d24d7981a67e59ef0da4d362698252f2137c
Ankur Singh <ankursinghchauhan10@gmail.com>
2018-03-25 12:52:02 +0530

    code format and UT

--- src/test/java/chauhan_traders/RunAllTests.java
+++ src/test/java/chauhan_traders/RunAllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DatabaseExecution.class, DatabaseDMLTest.class })
+@SuiteClasses({ DatabaseExecution.class, DatabaseDMLTest.class ,TestValidationMethods.class })
 public class RunAllTests {
 
 }
